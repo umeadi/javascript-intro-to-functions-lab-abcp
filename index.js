13dbc66e9b9aecdc15fa0bbfc438499cd7efd96a
Marlon Umeadi <umeadi@gmail.com>
2018-01-30 05:07:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  console.log string.
+}
+
+function logWhisper(string)
+{
+  console.logWhisper
+}
+
